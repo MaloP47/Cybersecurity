e98ca36670766d045f1a6d8a83000e63e6a579b0
Mallory PEULET <mpeulet@gmail.com>
2024-09-03 15:57:43 +0200

    Exercice 2 complete, need to do Ex1

--- scorpion/scorpion.py
+++ scorpion/scorpion.py
@@ -1,26 +1,31 @@
-from load_image import ft_load
 import sys
 from PIL import Image
 from PIL.ExifTags import TAGS
 
 
+def printExif(file):
+    try:
+        img = Image.open(file)
+        data = img._getexif()
+        if data is not None:
+            print(f"EXIF data for {file}:")
+            for tag, value in data.items():
+                tag_name = TAGS.get(tag, tag)
+                print(f"{tag_name}: {value}")
+        else:
+            print(f"No EXIF data found for {file}")
+    except Exception as e:
+        print(f"Error processing {file}: {e}")
+
+
 def main():
     try:
-        if len(sys.argv) == 2:
-            # print(ft_load(sys.argv[1]))
-            img = ft_load(sys.argv[1])
-            # img.show()
-            file = sys.argv[1]
-            data = img._getexif()
-            if data is not None:
-                print(f"EXIF data for {file}:")
-                for tag, value in data.items():
-                    tag_name = TAGS.get(tag, tag)
-                    print(f"{tag_name}: {value}")
-            else:
-                print(f"No EXIF data found for {file}")
+        if len(sys.argv) >= 2:
+            for file in sys.argv[1:]:
+                printExif(file)
+                print('************************************')
         else:
-            print("EXAMPLE")
+            print("Add a path! EXAMPLE: python scorpion.py ../img/animal.jpg")
     except Exception as e:
         print(f"Error: {e}")
 
